de754e49fb40b20a4284062976ddcf8529913579
MaxiCappe256 <maxicappe256@gmail.com>
2026-03-07 13:21:08 -0300

    fix use client directive

--- frontnend/app/appointments/new/page.tsx
+++ frontnend/app/appointments/new/page.tsx
@@ -1,4 +1,5 @@
-"use client";
+export const dynamic = "force-dynamic";
+("use client");
 import AppointmentForm from "@/components/AppointmentForm";
 import { useAppointmentsCreate } from "@/hooks/appointments/useAppointmentsCreate";
 import Link from "next/link";
