97aeec913fcd34ff0a7a6052872c8a989b000b37
MaxiCappe256 <maxicappe256@gmail.com>
2026-03-08 12:35:19 -0300

    age and phone changes

--- backend/src/clients/entities/client.entity.ts
+++ backend/src/clients/entities/client.entity.ts
@@ -1,11 +1,10 @@
-import { Appointment } from "src/appointments/entities/appointment.entity";
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Appointment } from 'src/appointments/entities/appointment.entity';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('clients')
 export class Client {
-
   @PrimaryGeneratedColumn()
-  id: number
+  id: number;
 
   @Column({ type: 'varchar', length: 60 })
   name: string;
@@ -13,14 +12,14 @@ export class Client {
   @Column({ type: 'varchar', length: 60 })
   surname: string;
 
-  @Column({ type: 'int' })
-  age: number;
+  @Column({ type: 'int', nullable: true })
+  age?: number;
 
   @Column({ type: 'varchar', length: 20, nullable: true })
-  phone: string;
+  phone?: string;
 
   @Column({ type: 'boolean', default: false })
-  paid: boolean
+  paid: boolean;
 
   @ManyToMany(() => Appointment, (appointment) => appointment.clients)
   appointments: Appointment[];
