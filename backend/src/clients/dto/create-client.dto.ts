cc69146033992a0ca26fcf7e5751a65d84120f50
MaxiCappe256 <maxicappe256@gmail.com>
2026-03-08 12:31:45 -0300

    remove age and phone

--- backend/src/clients/dto/create-client.dto.ts
+++ backend/src/clients/dto/create-client.dto.ts
@@ -1,28 +1,30 @@
-import { IsBoolean, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Length, Max, Min } from "class-validator";
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+  Max,
+  Min,
+} from 'class-validator';
 
 export class CreateClientDto {
-
-  @IsNotEmpty({ message: "El nombre es obligatorio" })
-  @IsString({ message: "El nombre debe ser un texto" })
-  @Length(3, 50, { message: "El nombre debe tener entre 3 y 50 caracteres" })
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  @IsString({ message: 'El nombre debe ser un texto' })
+  @Length(3, 50, { message: 'El nombre debe tener entre 3 y 50 caracteres' })
   name: string;
 
-  @IsNotEmpty({ message: "El apellido es obligatorio" })
-  @IsString({ message: "El apellido debe ser un texto" })
-  @Length(3, 50, { message: "El apellido debe tener entre 3 y 50 caracteres" })
+  @IsNotEmpty({ message: 'El apellido es obligatorio' })
+  @IsString({ message: 'El apellido debe ser un texto' })
+  @Length(3, 50, { message: 'El apellido debe tener entre 3 y 50 caracteres' })
   surname: string;
 
-  @IsNotEmpty({ message: "La edad es obligatoria" })
-  @IsInt({ message: 'La edad debe ser un número entero' })
-  @Min(10, { message: 'La edad mínima es 10' })
-  @Max(100, { message: 'La edad máxima es 100' })
-  age: number;
+  @IsOptional()
+  age?: number;
 
   @IsOptional()
-  @IsString({ message: 'El telefono debe ser texto' })
-  @Length(8, 15, {
-    message: 'El teléfono debe tener entre 8 y 15 caracteres',
-  })
   phone?: string;
 
   @IsOptional()
