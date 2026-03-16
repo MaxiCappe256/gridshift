e3ba036054e4028eba437684f7363e62222792b4
MaxiCappe256 <maxicappe256@gmail.com>
2026-03-16 17:15:37 -0300

    middlwrare

--- frontnend/middleware.ts
+++ frontnend/middleware.ts
@@ -1,28 +1,28 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  // 1. Usar nextUrl para obtener el pathname sin errores de tipos
-  const { pathname } = request.nextUrl;
-  const token = request.cookies.get("token")?.value;
+// export function middleware(request: NextRequest) {
+//   // 1. Usar nextUrl para obtener el pathname sin errores de tipos
+//   const { pathname } = request.nextUrl;
+//   const token = request.cookies.get("token")?.value;
 
-  // RUTAS PROTEGIDAS
-  const isProtectedRoute =
-    pathname.startsWith("/dashboard") || pathname.startsWith("/clients");
+//   // RUTAS PROTEGIDAS
+//   const isProtectedRoute =
+//     pathname.startsWith("/dashboard") || pathname.startsWith("/clients");
 
-  // Si no hay token y quiere entrar a una ruta protegida
-  if (isProtectedRoute && !token) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+//   // Si no hay token y quiere entrar a una ruta protegida
+//   if (isProtectedRoute && !token) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
 
-  // Si hay token e intenta ir al login (página raíz)
-  if (token && pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+//   // Si hay token e intenta ir al login (página raíz)
+//   if (token && pathname === "/") {
+//     return NextResponse.redirect(new URL("/dashboard", request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  // Evitamos que el middleware corra en archivos estáticos o internos de Next
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
+// export const config = {
+//   // Evitamos que el middleware corra en archivos estáticos o internos de Next
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };
